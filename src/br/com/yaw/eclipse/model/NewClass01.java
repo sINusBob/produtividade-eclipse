bb0cbb01761fe3dc4638de1e26aaec6b1bad95de
sINusBob <magno.alberto@live.com>
2015-01-22 16:30:40 -0200

    nova classe adicionada

---
+++ src/br/com/yaw/eclipse/model/NewClass01.java
@@ -0,0 +1,16 @@
+package br.com.yaw.eclipse.model;
+
+public class NewClass01 {
+	public String lala;
+	
+	public String getLala() {
+		return lala;
+	}
+	
+	public void setLala(String lala) {
+		this.lala = lala;
+	}
+	
+	
+
+}
